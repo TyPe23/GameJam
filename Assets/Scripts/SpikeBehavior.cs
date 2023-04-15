2241695609a907f92178d8754cccb4bb8c30c972
Ty Pederson <ty23117@gmail.com>
2023-04-15 13:50:11 -0500

    room prefabs, spike changes

--- Assets/Scripts/SpikeBehavior.cs
+++ Assets/Scripts/SpikeBehavior.cs
@@ -7,10 +7,11 @@ using UnityEngine;
 public class SpikeBehavior : MonoBehaviour
 {
     public GameObject spikeHB;
+    public int spikeDelayms;
 
     public async void ActiveSpike()
     {
-        await Task.Delay(100);
+        await Task.Delay(spikeDelayms);
         spikeHB.SetActive(true);
         await Task.Delay(1000);
         spikeHB.SetActive(false);
