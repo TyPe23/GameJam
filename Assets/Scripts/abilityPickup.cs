bca177e6a39d20cbead59a57c2fd729e66bf176e
JohnParks032 <80427077+JohnParks032@users.noreply.github.com>
2023-04-15 18:52:35 -0500

    added ability pickup sounds & added new test level

--- Assets/Scripts/abilityPickup.cs
+++ Assets/Scripts/abilityPickup.cs
@@ -14,6 +14,22 @@ public class abilityPickup : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             player.ability = ability;
+            if (ability == abilities.dash) {
+                Game.globalInstance.sndPlayer.PlaySound(SoundType.DASH_PICKUP, GetComponent<AudioSource>());
+            }
+            else if (ability == abilities.ice)
+            {
+                Game.globalInstance.sndPlayer.PlaySound(SoundType.ICE_PICKUP, GetComponent<AudioSource>());
+            }
+            else if (ability == abilities.fire)
+            {
+                Game.globalInstance.sndPlayer.PlaySound(SoundType.FIRE_PICKUP, GetComponent<AudioSource>());
+            }
+            else if (ability == abilities.attack)
+            {
+                Game.globalInstance.sndPlayer.PlaySound(SoundType.ATTACK, GetComponent<AudioSource>());
+            }
+
             Destroy(gameObject);
         }
     }
