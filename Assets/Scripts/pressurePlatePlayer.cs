d4ed401ab6291d457136e672fe12f0ba89f0575e
Ty Pederson <ty23117@gmail.com>
2023-04-16 00:39:42 -0500

    lvl 2 greybox

---
+++ Assets/Scripts/pressurePlatePlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePlatePlayer : MonoBehaviour
+{
+    public door[] doors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            foreach (door door in doors)
+            {
+                door.activated = true;
+            }
+        }
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("basicBlock"))
+    //    {
+    //        foreach (door door in doors)
+    //        {
+    //            door.activated = false;
+    //        }
+    //    }
+    //}
+}
