2ea92633c7a3ba77ceacc76e1f86c36b405f344c
Ty Pederson <ty23117@gmail.com>
2023-04-16 12:55:38 -0500

    pressure plate and lvl1 changes

--- Assets/Scripts/pressurePlate.cs
+++ Assets/Scripts/pressurePlate.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class pressurePlate : MonoBehaviour
 {
     public door[] doors;
+    private bool activated = false;
+    private float targetY;
+    private float origY;
+    public GameObject PressurePlate;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetY = PressurePlate.transform.position.y - 0.25f;
+        origY = PressurePlate.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (activated)
+        {
+            if (PressurePlate.transform.position.y > targetY)
+            {
+                PressurePlate.transform.Translate(Vector3.down * Time.deltaTime);
+            }
+            
+        }
+        else
+        {
+            if (PressurePlate.transform.position.y < origY)
+            {
+                PressurePlate.transform.Translate(Vector3.up * Time.deltaTime);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("basicBlock"))
         {
+            activated = true;
             foreach (door door in doors)
             {
                 door.activated = true;
@@ -33,6 +53,7 @@ public class pressurePlate : MonoBehaviour
     {
         if (other.gameObject.CompareTag("basicBlock"))
         {
+            activated = false;
             foreach (door door in doors)
             {
                 door.activated = false;
