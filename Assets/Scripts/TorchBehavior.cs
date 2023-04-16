2796c6c96bddf5711e8eee95911b4c45f3202531
Eruruw <spencerrochel@yahoo.com>
2023-04-16 14:33:51 -0500

    Adjusting level 2, fixing blocks

--- Assets/Scripts/TorchBehavior.cs
+++ Assets/Scripts/TorchBehavior.cs
@@ -6,11 +6,13 @@ public class TorchBehavior : MonoBehaviour
 {
     [SerializeField]
     private GameObject torchLight, particles;
+    private bool lit = false;
 
-    private void OnTriggerEnter(Collider col)
+    private void OnTriggerStay(Collider col)
     {
-        if(col.gameObject.CompareTag("fire"))
+        if(col.gameObject.CompareTag("fire") && !lit)
         {
+            lit = true;
             torchLight.SetActive(true);
             particles.SetActive(true);
         }
